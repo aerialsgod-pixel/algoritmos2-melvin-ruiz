50de071024aaa5b8c49829f5eb3ed0ce42305a3b
Leo1490 <aerialsgod@gmail.com>
2026-08-15 15:36:40 -0600

    commit inicial

---
+++ Semana02/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Semana02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Esta es la semana 2");
+
+        }
+    }
+}
